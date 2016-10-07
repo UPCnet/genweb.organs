6edce1a8b23f85fa7696006d5218341ff5b6b407
Roberto Diaz <robertodiaz76@gmail.com>
2016-10-07 13:53:58 +0200

    added logo to header

---
+++ genweb/organs/browser/viewlets.py
@@ -0,0 +1,142 @@
+# -*- coding: utf-8 -*-
+import requests
+import socket
+from five import grok
+from plone import api
+from time import time
+from Acquisition import aq_inner
+from AccessControl import getSecurityManager
+from zope.interface import Interface
+from zope.component import getMultiAdapter
+from zope.security import checkPermission
+
+from plone.memoize import ram
+from plone.memoize.view import memoize_contextless
+
+# from Products.CMFCore import permissions
+from Products.CMFCore.utils import getToolByName
+from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
+from zope.browserpage.viewpagetemplatefile import ViewPageTemplateFile as ZopeViewPageTemplateFile
+from Products.Five.browser.metaconfigure import ViewMixinForTemplates
+from Products.CMFPlone.interfaces import IPloneSiteRoot
+
+from plone.app.layout.viewlets.common import PersonalBarViewlet, GlobalSectionsViewlet, PathBarViewlet
+from plone.app.layout.viewlets.common import SearchBoxViewlet, ManagePortletsFallbackViewlet
+from plone.app.layout.viewlets.interfaces import IPortalTop, IPortalHeader, IBelowContent
+from plone.app.layout.viewlets.interfaces import IPortalFooter
+from plone.app.layout.navigation.interfaces import INavigationRoot
+
+from genweb.core import HAS_CAS
+from genweb.core import HAS_PAM
+from genweb.core.interfaces import IHomePage
+from genweb.theme.browser.interfaces import IHomePageView
+from genweb.core.utils import genweb_config
+from genweb.core.utils import havePermissionAtRoot
+from genweb.core.utils import pref_lang
+from genweb.theme.browser.interfaces import IGenwebTheme
+from genweb.core.browser.viewlets import gwCSSViewletManager
+from genweb.core.browser.viewlets import baseResourcesViewlet
+from genweb.organs.interfaces import IGenwebOrgansLayer
+
+
+grok.context(Interface)
+
+
+class viewletBase(grok.Viewlet):
+    grok.baseclass()
+
+    @memoize_contextless
+    def root_url(self):
+        return self.portal().absolute_url()
+
+    @memoize_contextless
+    def portal(self):
+        return api.portal.get()
+
+    def genweb_config(self):
+        return genweb_config()
+
+    def pref_lang(self):
+        """ Extracts the current language for the current user """
+        lt = getToolByName(self.portal(), 'portal_languages')
+        return lt.getPreferredLanguage()
+
+
+class gwHeader(viewletBase):
+    grok.name('genweb.header')
+    grok.template('header')
+    grok.viewletmanager(IPortalHeader)
+    grok.layer(IGenwebOrgansLayer)
+
+
+    def get_image_class(self):
+        if self.genweb_config().treu_menu_horitzontal:
+            # Is a L2 type
+            return 'l2-image'
+        else:
+            return 'l3-image'
+
+    def show_login(self):
+        isAnon = getMultiAdapter((self.context, self.request), name='plone_portal_state').anonymous()
+        return not self.genweb_config().amaga_identificacio and isAnon
+
+    # def show_directory(self):
+    #     return self.genweb_config().directori_upc
+
+    def show_directory(self):
+        show_general = self.genweb_config().directori_upc
+        return show_general
+
+    def show_directory_filtered(self):
+        show_filtered = self.genweb_config().directori_filtrat
+        return show_filtered
+
+    def getURLDirectori(self, codi):
+        if codi:
+            return "http://directori.upc.edu/directori/dadesUE.jsp?id=%s" % codi
+        else:
+            return "http://directori.upc.edu"
+
+    def get_title(self):
+        title = getattr(self.genweb_config(), 'html_title_{}'.format(self.pref_lang()))
+        if title:
+            return title
+        else:
+            return u''
+
+    def is_logo_enabled(self):
+        return self.genweb_config().right_logo_enabled
+
+    def get_right_logo_alt(self):
+        return self.genweb_config().right_logo_alt
+
+    def is_pam_installed(self):
+        return HAS_PAM
+
+    def getCustomLink(self):
+        """ Custom links """
+        lang = self.pref_lang()
+        custom_links = {
+            "ca": {
+                "cl_title": self.genweb_config().cl_title_ca,
+                "url": self.genweb_config().cl_url_ca,
+                "image": self.genweb_config().cl_img_ca,
+                "oinw": self.genweb_config().cl_open_new_window_ca,
+                "enable": self.genweb_config().cl_enable_ca,
+                },
+            "es": {
+                "cl_title": self.genweb_config().cl_title_es,
+                "url": self.genweb_config().cl_url_es,
+                "image": self.genweb_config().cl_img_es,
+                "oinw": self.genweb_config().cl_open_new_window_es,
+                "enable": self.genweb_config().cl_enable_es,
+                },
+            "en": {
+                "cl_title": self.genweb_config().cl_title_en,
+                "url": self.genweb_config().cl_url_en,
+                "image": self.genweb_config().cl_img_en,
+                "oinw": self.genweb_config().cl_open_new_window_en,
+                "enable": self.genweb_config().cl_enable_en,
+                },
+            }
+        return custom_links[lang]
--- genweb/organs/interfaces.py
+++ genweb/organs/interfaces.py
@@ -1,8 +1,9 @@
 # -*- coding: utf-8 -*-
 """Module where all interfaces, events and exceptions live."""
 
+from genweb.theme.browser.interfaces import IGenwebTheme
 from plone.theme.interfaces import IDefaultPloneLayer
 
 
-class IGenwebOrgansLayer(IDefaultPloneLayer):
+class IGenwebOrgansLayer(IGenwebTheme): 
     """Marker interface that defines a Zope 3 browser layer."""
