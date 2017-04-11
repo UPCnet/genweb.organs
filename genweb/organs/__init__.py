71a083702ff4b0352731900da475373a362212e7
roberto.diaz <roberto.diaz@upcnet.es>
2017-04-11 14:38:19 +0200

    added translations to change estate

--- genweb/organs/__init__.py
+++ genweb/organs/__init__.py
@@ -4,6 +4,7 @@
 from zope.i18nmessageid import MessageFactory
 
 _ = MessageFactory('genweb.organs')
+_GW = MessageFactory('genweb')
 
 
 def initialize(context):
--- genweb/organs/browser/events/change.py
+++ genweb/organs/browser/events/change.py
@@ -2,6 +2,7 @@
 from zExceptions import Redirect
 from genweb.organs.utils import addEntryLog
 from genweb.organs import _
+from genweb.organs import _GW
 
 
 def sessio_changed(session, event):
@@ -10,10 +11,10 @@ def sessio_changed(session, event):
     """
     # si passem estat a convocat cal enviar mail de convocatoria...
     try:
-        old = _(str(event.status['review_state']))
-        new = _(str(event.transition.new_state_id))
-        message = str(old) + ' → ' + str(new)
-        addEntryLog(session, None, _(u'Changed workflow state'), message)  # add log
+        # old = _GW(event.status['review_state'])
+        new = _GW(event.transition.new_state_id)
+        # message = (old) + ' →2 ' + (new)
+        addEntryLog(session, None, _(u'Changed workflow state'), new)  # add log
     except:
         addEntryLog(session, None, _(u'New session created'), session.Title())  # add log
 
