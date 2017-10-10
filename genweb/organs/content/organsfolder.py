3e6d68a96e03ad30db1716fbcb2def9c843367ec
Roberto Diaz <roberto.diaz@upcnet.es>
2017-10-10 17:38:16 +0200

    list organs in folder based on permissions

--- genweb/organs/content/organsfolder.py
+++ genweb/organs/content/organsfolder.py
@@ -1,15 +1,17 @@
 # -*- coding: utf-8 -*-
 from five import grok
+from genweb.organs import _
+from genweb.organs import utils
 from plone.directives import form
 from plone.namedfile.field import NamedBlobImage
 from Products.CMFCore.utils import getToolByName
-from genweb.organs import _
 from zope import schema
+
 grok.templatedir("templates")
 
 
 class IOrgansfolder(form.Schema):
-    """ Tipus Organs Folder: Carpeta que conté Organs
+    """ Organs Folder: Carpeta Unitat que conté Organs de Govern
     """
 
     customImage = schema.Bool(
@@ -35,7 +37,7 @@ class View(grok.View):
         """
         portal_catalog = getToolByName(self, 'portal_catalog')
         folder_path = '/'.join(self.context.getPhysicalPath())
-        values = portal_catalog.unrestrictedSearchResults(
+        values = portal_catalog.searchResults(
             portal_type='genweb.organs.organgovern',
             sort_on='getObjPositionInParent',
             path={'query': folder_path,
@@ -43,11 +45,30 @@ class View(grok.View):
 
         results = []
         for obj in values:
-            # value = obj.getObject()
-            value = obj._unrestrictedGetObject()
-            results.append(dict(title=value.title,
-                                absolute_url=value.absolute_url(),
-                                acronim=value.acronim,
-                                organType=value.organType,
-                                review_state=obj.review_state))
+            value = obj.getObject()
+            organType = value.organType
+            # If Manager or Obert bypass and list all
+            if utils.isManager(self) or (organType == 'open_organ'):
+                results.append(dict(title=value.title,
+                                    absolute_url=value.absolute_url(),
+                                    acronim=value.acronim,
+                                    organType=value.organType,
+                                    review_state=obj.review_state))
+            # if restricted_to_members_organ
+            elif organType == 'restricted_to_members_organ':
+                if (utils.isSecretari(self) or utils.isEditor(self) or utils.isMembre(self)):
+                    results.append(dict(title=value.title,
+                                        absolute_url=value.absolute_url(),
+                                        acronim=value.acronim,
+                                        organType=value.organType,
+                                        review_state=obj.review_state))
+            # if restricted_to_affected_organ
+            elif organType == 'restricted_to_affected_organ':
+                if (utils.isSecretari(self) or utils.isEditor(self) or utils.isMembre(self) or utils.isAfectat(self)):
+                    results.append(dict(title=value.title,
+                                        absolute_url=value.absolute_url(),
+                                        acronim=value.acronim,
+                                        organType=value.organType,
+                                        review_state=obj.review_state))
+
         return results
