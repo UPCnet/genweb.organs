e65a59322331fcb2919d6fe08b5f6e38b7399306
Roberto Diaz <robertodiaz76@gmail.com>
2017-10-31 17:25:44 +0100

    added html2pdf, testing in acta print

--- setup.py
+++ setup.py
@@ -31,6 +31,7 @@ setup(name='genweb.organs',
       zip_safe=False,
       install_requires=[
           'setuptools',
+          'html2text'
       ],
       extras_require={'test': ['plone.app.testing',
                                'plone.app.testing[robot]>=4.2.2',
