b3256d5b2d6c27f1535d6da9505d46f1f75bb563
Roberto Diaz <roberto.diaz@upcnet.es>
2017-12-20 13:38:59 +0100

    ensure disable webservice

--- genweb/organs/browser/webservice.py
+++ genweb/organs/browser/webservice.py
@@ -6,33 +6,43 @@ from Products.CMFCore.utils import getToolByName
 class webservice(BrowserView):
     # TODO: Enable it and make fully functional...
     def __call__(self):
+        # Execute on -- Organsfolder (genweb.organs.organsfolder) --
         # http://localhost:8080/organs/ca/eetac/consell-de-govern/ws?id=CDG/2017/01/01
-        # CDG/2017/01/01
+        # CG/2017/01/01
         # http://localhost:8080/organs/ca/eetac/consell-de-govern/session1/informe-del-rector
         # Maybe better... http://localhost:8080/organs/api/CDG/2017/01/01
+
+        return None
+
         itemid = self.request.form.get('id')
         if itemid == '':
             pass
         else:
             item = itemid.split('/')
-            organ = item[0]
-            year = item[1]
-            session = item[2]
-            acord = item[3]
+            acronim = item[0]
+            # year = item[1]
+            num_session = item[2]
+            # acord = item[3]
 
             portal_catalog = getToolByName(self, 'portal_catalog')
-            organPath = portal_catalog.searchResults(
-                portal_type=['genweb.organs.organgovern'],
-                acronim=organ)[0].getPath()
+            organs = portal_catalog.searchResults(
+                portal_type=['genweb.organs.organgovern'])
 
-            sessionPath = portal_catalog.searchResults(
+            for org in organs:
+                if org.getObject().acronim == acronim:
+                    organPath = org.getPath()
+
+            sessions = portal_catalog.searchResults(
                 portal_type=['genweb.organs.sessio'],
                 path={'query': organPath,
-                      'depth': 1})[0].getPath()
+                      'depth': 1})
+            for sess in sessions:
+                if sess.getObject().numSession == num_session:
+                    sessionPath = sess.getPath()
 
             punt = portal_catalog.searchResults(
                 portal_type=['genweb.organs.punt', 'genweb.organs.acord'],
                 path={'query': sessionPath,
                       'depth': 1})[0]
-                    #   .getPath()
+
             self.request.response.redirect(punt.getURL())
