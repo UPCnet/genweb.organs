89be7859067516fae744bb5016d761615749b11e
Roberto Diaz <roberto.diaz@upcnet.es>
2018-06-22 13:16:57 +0200

    added translation

--- genweb/organs/locales/update.sh
+++ genweb/organs/locales/update.sh
@@ -2,7 +2,7 @@
 cd ..
 cd ..
 cd ..
-../../bin/i18ndude rebuild-pot --pot genweb/organs/locales/genweb.organs.pot --create genweb.organs .
+../../bin/i18ndude rebuild-pot --pot genweb/organs/locales/genweb.organs.pot --create genweb.organs . --exclude="eggs"
 cd genweb/organs/locales/ca/LC_MESSAGES
 ../../../../../../../bin/i18ndude sync --pot ../../genweb.organs.pot genweb.organs.po
 cd ..
