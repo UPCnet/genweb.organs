1ae362d8b3b7c4d7843ac46788b7abb49cd9768e
ilopezsmx <ilopezsmx2011@gmail.com>
2021-12-21 12:52:21 +0100

    Back to development: 0.53

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from setuptools import setup, find_packages
 import os
 
-version = '0.52'
+version = '0.53.dev0'
 
 README = open("README.rst").read()
 HISTORY = open(os.path.join("docs", "HISTORY.rst")).read()
