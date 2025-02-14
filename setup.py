7f0ea907e0e343bb485841cfac86fce7888d241f
Iago López <ilopezsmx2011@gmail.com>
2025-02-14 11:12:25 +0100

    Back to development: 0.70

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from setuptools import setup, find_packages
 import os
 
-version = '0.69'
+version = '0.70.dev0'
 
 README = open("README.rst").read()
 HISTORY = open(os.path.join("docs", "HISTORY.rst")).read()
