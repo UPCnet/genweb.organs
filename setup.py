d96196cb8b774b368103a23fad48531a553aa80e
Pilar Marinas <pilar.marinas@upcnet.es>
2025-05-12 15:46:15 +0200

    Vibe coding actualizar plantillas bootstrap 5

--- setup.py
+++ setup.py
@@ -7,40 +7,51 @@ version = '0.73.dev0'
 README = open("README.rst").read()
 HISTORY = open(os.path.join("docs", "HISTORY.rst")).read()
 
-setup(name='genweb.organs',
-      version=version,
-      description="Paquet Organs de Govern amb jQuery i que s'integra a Genweb.",
-      long_description=README + "\n" + HISTORY,
-      classifiers=[
-          "Environment :: Web Environment",
-          "Framework :: Plone",
-          "Operating System :: OS Independent",
-          "Programming Language :: Python",
-          "Programming Language :: Python :: 2.6",
-          "Programming Language :: Python :: 2.7",
-          "Topic :: Software Development :: Libraries :: Python Modules",
-      ],
-      keywords='organs govern genweb jquery',
-      author='plone.team@upcnet.es',
-      author_email='plone.team@upcnet.es',
-      url='https://github.com/UPCnet/genweb.organs',
-      license='GPL',
-      packages=find_packages(exclude=['ez_setup']),
-      namespace_packages=['genweb'],
-      include_package_data=True,
-      zip_safe=False,
-      install_requires=[
-          'setuptools',
-          'collective.dexteritytextindexer',
-          'selenium',
-          'pdfkit'
-      ],
-      extras_require={'test': ['plone.app.testing',
-                               'plone.app.testing[robot]>=4.2.2',
-                               'plone.app.robotframework[debug]']},
-      entry_points="""
-      # -*- Entry points: -*-
-      [z3c.autoinclude.plugin]
-      target = plone
-      """,
-      )
+setup(
+    name='genweb.organs',
+    version=version,
+    description="Paquet Organs de Govern amb jQuery i que s'integra a Genweb.",
+    long_description=README + "\n" + HISTORY,
+    classifiers=[
+        "Environment :: Web Environment",
+        "Framework :: Plone :: 6.0",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Operating System :: OS Independent",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+    keywords='organs govern genweb',
+    author='plone.team@upcnet.es',
+    author_email='plone.team@upcnet.es',
+    url='https://github.com/UPCnet/genweb.organs',
+    license='GPL',
+    packages=find_packages(exclude=['ez_setup']),
+    namespace_packages=['genweb'],
+    include_package_data=True,
+    zip_safe=False,
+    python_requires='>=3.8',
+    install_requires=[
+        'setuptools',
+        'Plone >=6.0.0',
+        'plone.app.dexterity',
+        'plone.app.contenttypes',
+        'plone.app.registry',
+        'plone.api',
+        'collective.dexteritytextindexer',
+        'pdfkit',
+    ],
+    extras_require={
+        'test': [
+            'plone.app.testing',
+            'plone.app.robotframework[debug]',
+        ]
+    },
+    entry_points="""
+    [z3c.autoinclude.plugin]
+    target = plone
+    """,
+)
