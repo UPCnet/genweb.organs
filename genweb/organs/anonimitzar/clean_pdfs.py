0a7b5c2df8a2bbaadfd4e1916a77d358c2b1c7b0
Clàudia Andreu <claudia.andreu@ithinkupc.com>
2025-06-13 14:02:31 +0200

    [ADD] view for anonymising files

--- genweb/organs/anonimitzar/clean_pdfs.py
+++ genweb/organs/anonimitzar/clean_pdfs.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
 from Products.Five import BrowserView
@@ -6,7 +7,7 @@ from plone.registry.interfaces import IRegistry
 from zope.component import getUtility
 from plone.protect.interfaces import IDisableCSRFProtection
 from zope.interface import alsoProvides
-from genweb.organs.anonimitzar.controlpanel import IAnonimitzarSettings 
+from genweb.organs.anonimitzar.controlpanel import IAnonimitzarSettings
 from plone.namedfile.file import NamedBlobFile  
 
 import requests
@@ -24,7 +25,7 @@ def is_signed_pdf(data):
             acroform = reader.trailer['/Root']['/AcroForm']
             if '/Fields' in acroform:
                 for field in acroform['/Fields']:
-                    field_obj = field.get_object()
+                    field_obj = field.getObject()
                     if field_obj.get('/FT') == '/Sig':
                         return True
         return False
@@ -40,7 +41,14 @@ class CleanPDFsView(BrowserView):
         alsoProvides(self.request, IDisableCSRFProtection)
 
         catalog = api.portal.get_tool('portal_catalog')
-        brains = catalog.searchResults(portal_type='File')
+        brains = catalog.searchResults(portal_type=[
+            'File',
+            'genweb.organs.file',
+            'genweb.organs.acta',
+            'genweb.organs.annex',
+            'genweb.organs.propostapunt',
+            'genweb.organs.proposar_punt'
+        ])
 
         registry = getUtility(IRegistry)
         settings = registry.forInterface(IAnonimitzarSettings, check=False)
@@ -58,9 +66,17 @@ class CleanPDFsView(BrowserView):
         count_signed = 0
         errors = []
 
+        possible_pdf_fields = [
+            'file', 'visiblefile', 'hiddenfile', 'signsFile', 'anexFile', 'acta'
+        ]
+
+
         for brain in brains:
             obj = brain.getObject()
 
+            if obj is None:
+                logger.warning("No s'ha pogut recuperar l'objecte per %s", brain.getPath())
+                continue
             # Reparar obj.file si és bytes (cas erroni)
             # if isinstance(obj.file, bytes):
             #     filename = obj.getId() + '.pdf'
@@ -71,61 +87,66 @@ class CleanPDFsView(BrowserView):
             #     )
             #     obj.reindexObject()
 
-            if not obj.file or not obj.file.filename.lower().endswith('.pdf'):
-                continue
+            for fieldname in possible_pdf_fields:
+                file_field = getattr(obj, fieldname, None)
+                if not file_field:
+                    continue
 
-            file_data = obj.file.data
+                try:
+                    filename = getattr(file_field, 'filename', None)
+                    if not filename or not filename.lower().endswith('.pdf'):
+                        continue
 
-            if is_signed_pdf(file_data):
-                logger.info("[SKIPPED] %s - PDF signat" % obj.absolute_url())
-                count_signed += 1
-                continue
+                    file_data = file_field.data
+                    if is_signed_pdf(file_data):
+                        logger.info("[SKIPPED] {} [{}] - PDF signat".format(obj.absolute_url(), fieldname))
+                        count_signed += 1
+                        continue
 
-            count_total += 1
+                    count_total += 1
 
-            try:
-                filename = obj.file.filename
+                    files = {
+                        'fitxerPerAnonimitzar': (filename, file_data, 'application/pdf')
+                    }
 
-                files = {
-                    'fitxerPerAnonimitzar': (filename, file_data, 'application/pdf')
-                }
+                    response = requests.post(api_url, headers=headers, files=files)
 
-                response = requests.post(api_url, headers=headers, files=files)
+                    if response.status_code == 200:
+                        cleaned_data = response.content
 
-                if response.status_code == 200:
-                    cleaned_data = response.content
+                        setattr(obj, fieldname, NamedBlobFile(
+                            data=cleaned_data,
+                            contentType='application/pdf',
+                            filename=filename
+                        ))
+                        logger.info("[OK] {} [{}]".format(obj.absolute_url(), fieldname))
+                        count_cleaned += 1
+                    else:
+                        error_msg = "{} [{}]: {}".format(obj.absolute_url(), fieldname, response.status_code)
+                        errors.append(error_msg)
+                        logger.exception("[ERROR] {}".format(obj.absolute_url()))
 
-                    obj.file = NamedBlobFile(
-                        data=cleaned_data,
-                        contentType='application/pdf',
-                        filename=filename
-                    )
+                except Exception as e:
+                    error_msg = "{} [{}]: {}".format(obj.absolute_url(), fieldname, str(e))
+                    errors.append(error_msg)
+                    logger.exception("[ERROR] {}".format(obj.absolute_url()))
 
-                    obj.reindexObject()
-                    count_cleaned += 1
-                    logger.info("[OK] %s" % obj.absolute_url())
-                else:
-                    errors.append("%s: %s" % (obj.absolute_url(), response.status_code))
-                    logger.warning("[FAIL] %s - %s" % (obj.absolute_url(), response.status_code))
-
-            except Exception as e:
-                errors.append("%s: %s" % (obj.absolute_url(), str(e)))
-                logger.exception("[ERROR] %s" % obj.absolute_url())
+            obj.reindexObject()
 
         html = """
-                <h2>PDF Metadata Cleanup</h2>
-                <p>Total PDFs candidates: <strong>{total_candidates}</strong></p>
-                <p>Skipped (signed): <strong>{signed_count}</strong></p>
-                <p>Successfully cleaned: <strong>{cleaned_count}</strong></p>
-                <p>Errors: <strong>{error_count}</strong></p>
-                <pre>{errors_list}</pre>
-            """.format(
-                total_candidates=count_total + count_signed,
-                signed_count=count_signed,
-                cleaned_count=count_cleaned,
-                error_count=len(errors),
-                errors_list='<br>'.join(errors)
-            )
+            <h2>PDF Metadata Cleanup</h2>
+            <p>Total PDFs candidates: <strong>{total}</strong></p>
+            <p>Skipped (signed): <strong>{signed}</strong></p>
+            <p>Successfully cleaned: <strong>{cleaned}</strong></p>
+            <p>Errors: <strong>{errors}</strong></p>
+            <pre>{error_list}</pre>
+        """.format(
+            total=count_total + count_signed,
+            signed=count_signed,
+            cleaned=count_cleaned,
+            errors=len(errors),
+            error_list='<br>'.join(errors)
+        )
 
         transaction.commit()
         self.request.response.setHeader("Content-Type", "text/html; charset=utf-8")
--- genweb/organs/subscribers.py
+++ genweb/organs/subscribers.py
@@ -1,8 +1,16 @@
+# -*- coding: utf-8 -*-
+
 from five import grok
 from zope.lifecycleevent.interfaces import IObjectRemovedEvent
 from zope.lifecycleevent.interfaces import IObjectCreatedEvent
 from Products.CMFCore.interfaces import IActionSucceededEvent
-
+from plone.dexterity.interfaces import IDexterityContent
+from zope.component import adapter
+from plone.namedfile.file import NamedBlobFile
+from zope.lifecycleevent.interfaces import IObjectAddedEvent
+from zope.component import getUtility
+from plone.registry.interfaces import IRegistry
+from genweb.organs.anonimitzar.controlpanel import IAnonimitzarSettings
 from genweb.organs.utils import get_settings_property
 from genweb.organs.content.organgovern import IOrgangovern
 from genweb.organs.content.sessio import ISessio
@@ -10,6 +18,12 @@ from genweb.organs.content.acord import IAcord
 from genweb.organs.indicators.updating import (
     update_indicators,
     update_indicators_if_state)
+from PyPDF2 import PdfFileReader
+from io import BytesIO
+import logging
+logger = logging.getLogger(__name__)
+import requests
+
 
 
 # @grok.subscribe(IOrgangovern, IObjectRemovedEvent)
@@ -49,3 +63,70 @@ def update_indicators_on_acord_deletion(obj, event):
 def update_indicators_on_acord_review_state_change(obj, event):
     update_indicators(
         obj, service=get_settings_property('service_id'), indicator='acord-n')
+
+def is_signed_pdf(data):
+    try:
+        reader = PdfFileReader(BytesIO(data))
+        if '/AcroForm' in reader.trailer['/Root']:
+            acroform = reader.trailer['/Root']['/AcroForm']
+            if '/Fields' in acroform:
+                for field in acroform['/Fields']:
+                    field_obj = field.gteObject()
+                    if field_obj.get('/FT') == '/Sig':
+                        return True
+        return False
+    except Exception as e:
+        logger.warning("Error analizando firma en PDF: %s" % e)
+        return False
+
+
+@grok.subscribe(IDexterityContent, IObjectAddedEvent)
+def clean_pdf_on_upload(obj, event):
+    """Subscriber que limpia el PDF al subirlo si no está firmado."""
+    if not getattr(obj, 'file', None):
+        return
+
+    if not obj.file.filename.lower().endswith('.pdf'):
+        return
+
+    file_data = obj.file.data
+
+    if is_signed_pdf(file_data):
+        logger.info("[SKIPPED] %s - PDF signat" % obj.absolute_url())
+        return
+
+    try:
+        registry = getUtility(IRegistry)
+        settings = registry.forInterface(IAnonimitzarSettings, check=False)
+
+        api_url = settings.api_url
+        api_key = settings.api_key
+
+        headers = {
+            'accept': 'application/json;charset=utf-8',
+            'X-Api-Key': api_key
+        }
+
+        filename = obj.file.filename
+        files = {
+            'fitxerPerAnonimitzar': (filename, file_data, 'application/pdf')
+        }
+
+        response = requests.post(api_url, headers=headers, files=files)
+
+        if response.status_code == 200:
+            cleaned_data = response.content
+
+            obj.file = NamedBlobFile(
+                data=cleaned_data,
+                contentType='application/pdf',
+                filename=filename
+            )
+
+            obj.reindexObject()
+            logger.info("[OK] %s - PDF anonimizado" % obj.absolute_url())
+        else:
+            logger.warning("[FAIL] %s - %s - %s" % (obj.absolute_url(), response.status_code, response.text))
+
+    except Exception as e:
+        logger.exception("[ERROR] %s - %s" % (obj.absolute_url(), str(e))) 
