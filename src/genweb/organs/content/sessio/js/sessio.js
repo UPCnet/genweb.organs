da5749997f408310582df138a3bfcb61c20eda22
Iago <ilopezsmx2011@gmail.com>
2025-07-15 10:53:41 +0200

    [UPD] Butlleti

--- src/genweb/organs/content/sessio/js/sessio.js
+++ src/genweb/organs/content/sessio/js/sessio.js
@@ -7,6 +7,23 @@ $(document).ready(function(){
     if (refreshNeeded) { setTimeout(() => window.location.reload(), 500); }
   });
 
+  // Print Acta
+  var $btnPrint = $('#printButlletiBtn');
+  var $iframe = $('#printButlletiIframe');
+  if ($btnPrint.length && $iframe.length) {
+    $btnPrint.on('click', function (e) {
+      e.preventDefault();
+      var url = $btnPrint.data('url');
+      $iframe.attr('src', url + '/butlleti');
+      $iframe.off('load').on('load', function () {
+        setTimeout(function () {
+          $iframe[0].contentWindow.focus();
+          $iframe[0].contentWindow.print();
+        }, 200);
+      });
+    });
+  }
+
   // Para el desplegable de información de voto público
   //$(".openInfo").click(function () {
   //  $(this).toggleClass("opened");
