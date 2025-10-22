0a1305c6663fa8f7063a29cee00bc68df2ff5c71
Iago López <ilopezsmx2011@gmail.com>
2025-10-22 10:35:51 +0200

    [UPD] No contemplar ficheros sin contenido

--- genweb/organs/utils.py
+++ genweb/organs/utils.py
@@ -628,4 +628,4 @@ def getFilesSessio(context):
             for file in files_subpunt:
                 files.append(file.getObject())
 
-    return files
+    return [file for file in files if file.visiblefile or file.hiddenfile]
